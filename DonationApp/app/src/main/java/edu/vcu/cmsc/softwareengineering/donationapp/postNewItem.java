f8751d27cce94e2bfe6a6c9ea33ef5e9a8d30614
caadzima <caadzima@gmail.com>
2020-03-03 20:42:23 -0500

    Created activity for posting a new item

---
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/postNewItem.java
@@ -0,0 +1,16 @@
+// Project B Team 1 Spring 2020
+package edu.vcu.cmsc.softwareengineering.donationapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// Activity for having a donor post a new item for donation
+public class postNewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_new_item);
+    }
+}
