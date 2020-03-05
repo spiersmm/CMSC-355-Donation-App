3fe3b3b414c9216d136f41dd44bacfca35abd1e2
caadzima <caadzima@gmail.com>
2020-03-04 21:39:18 -0500

    Created drop down menus for item postings

--- DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/postNewItem.java
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/postNewItem.java
@@ -2,6 +2,12 @@
 package edu.vcu.cmsc.softwareengineering.donationapp;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +18,75 @@ public class postNewItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_new_item);
+
+        createCategorySpinner();
+        createDeliverySpinner();
+        createConditionSpinner();
+        createQuantitySpinnter();
     }
+
+    public void createCategorySpinner() {
+        Spinner categories = (Spinner) findViewById(R.id.spinnerCategories);
+
+        List<String> categoryItems = new ArrayList<String>();
+        categoryItems.add("Clothes");
+        categoryItems.add("Food");
+        categoryItems.add("Furniture");
+        categoryItems.add("Toiletries");
+        categoryItems.add("Games/Toys");
+        categoryItems.add("Books");
+        categoryItems.add("Other");
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, categoryItems);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categories.setAdapter(categoryAdapter);
+        //categories.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+    }
+
+    public void createDeliverySpinner() {
+        Spinner delivery = (Spinner) findViewById(R.id.spinnerDelivery);
+
+        List<String> deliveryItems = new ArrayList<String>();
+        deliveryItems.add("Pickup");
+        deliveryItems.add("Delivery");
+
+        ArrayAdapter<String> deliveryAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, deliveryItems);
+        deliveryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        delivery.setAdapter(deliveryAdapter);
+    }
+
+    public void createConditionSpinner() {
+        Spinner condition = (Spinner) findViewById(R.id.spinnerCondition);
+
+        List<String> Items = new ArrayList<String>();
+        Items.add("Perfect");
+        Items.add("Ok");
+        Items.add("Poor");
+
+        ArrayAdapter<String> conditionAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, Items);
+        conditionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        condition.setAdapter(conditionAdapter);
+    }
+
+    public void createQuantitySpinnter() {
+        Spinner quantity = (Spinner) findViewById(R.id.spinnerQuantity);
+
+        List<String> Items = new ArrayList<String>();
+        Items.add("1");
+        Items.add("2");
+        Items.add("3");
+        Items.add("4");
+        Items.add("5");
+        Items.add("Over 5");
+
+        ArrayAdapter<String> QuantityAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, Items);
+        QuantityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        quantity.setAdapter(QuantityAdapter);
+    }
+
+
 }
