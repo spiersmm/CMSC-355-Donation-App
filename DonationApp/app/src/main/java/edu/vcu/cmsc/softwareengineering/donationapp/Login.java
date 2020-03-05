5ca9634db42f652e54edbb059a071014f311d52f
caadzima <caadzima@gmail.com>
2020-03-04 19:12:14 -0500

    Connected login to donor main activity screen

--- DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/Login.java
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import edu.vcu.cmsc.softwareengineering.donationapp.data.LoginData;
 
@@ -12,6 +13,8 @@ import edu.vcu.cmsc.softwareengineering.donationapp.data.LoginData;
  * currently this class starts the app. The data entered by the user is handled
  * in a 'LoginData' object.
  */
+
+
 public class Login extends AppCompatActivity {
 	
 	private LoginData data;
@@ -20,31 +23,29 @@ public class Login extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		Button loginButton = findViewById(R.id.loginButton);
 		
-		 data = new LoginData(findViewById(R.id.usernameEditText),
-					 findViewById(R.id.passwordEditText),
-					 findViewById(R.id.loginButton));
+		// data = new LoginData(findViewById(R.id.usernameEditText),
+		//			 findViewById(R.id.passwordEditText),
+		//			 findViewById(R.id.loginButton));
 		 
-		 data.setLoginListener(new View.OnClickListener() {
-			 @Override
-			 public void onClick(View v) {
-				onLogin(v);
-			 }
-		 });
+		loginButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				//if (data.goesToDonor()) {
+				Intent leaveLogin = new Intent(getApplicationContext(), DonorMain.class);
+				//} else {
+				//	intent = new Intent(this, PostListActivity.class);
+				//}
+
+				//	intent.putExtra(this.getPackageName(), data.getArgs());
+				startActivity(leaveLogin);
+			}
+		});
 	}
 	
-	public void onLogin(View view) {
-		Intent intent;
-		if (data.goesToDonor()) {
-			intent = new Intent(this, DonorMain.class);
-		} else {
-			intent = new Intent(this, PostListActivity.class);
-		}
-		
-		intent.putExtra(this.getPackageName(), data.getArgs());
-		startActivity(intent);
-		
-	}
+
 	
 	
 }
