7c671892d826c05ca4e0b4773d1048562b3365a5
caadzima <caadzima@gmail.com>
2020-03-04 20:38:27 -0500

    Connected donor main activity to post new item activity

--- DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/DonorMain.java
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/DonorMain.java
@@ -3,6 +3,9 @@ package edu.vcu.cmsc.softwareengineering.donationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 
 public class DonorMain extends AppCompatActivity {
 	
@@ -10,5 +13,15 @@ public class DonorMain extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_donor_main);
+
+		Button postItemButton = findViewById(R.id.postItem);
+
+		postItemButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent postNewItem = new Intent(getApplicationContext(), postNewItem.class);
+				startActivity(postNewItem);
+			}
+		});
 	}
 }
