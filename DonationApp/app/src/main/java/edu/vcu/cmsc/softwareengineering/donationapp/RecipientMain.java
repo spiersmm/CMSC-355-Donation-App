a709ef794f1c2750374d00578b05c87b6070ee37
Matt <scottm8@vcu.edu>
2020-03-19 13:56:43 -0400

    Created initial functionality / template for item record spinner

--- DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/RecipientMain.java
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/RecipientMain.java
@@ -5,12 +5,54 @@ package edu.vcu.cmsc.softwareengineering.donationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipientMain extends AppCompatActivity {
+
+	private String SelectedItemRecord;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recipient_main);
-	}
-}
+
+
+	createItemRecordSpinner();
+
+	} // end onCreate()
+
+
+
+
+	public void createItemRecordSpinner() {
+		final Spinner itemRecord = findViewById(R.id.ItemRecord);
+
+		List<String> itemRecordItems = new ArrayList<>();
+			itemRecordItems.add("Available");
+			itemRecordItems.add("Recieved");
+
+		ArrayAdapter<String> itemRecordAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, itemRecordItems);
+			itemRecordAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+			itemRecord.setAdapter(itemRecordAdapter);
+			itemRecord.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+					/* TODO */
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> parent) {
+					/* TODO */
+				}
+			});
+	} // end createItemRecordSpinner()
+
+
+} // end class
