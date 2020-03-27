71671cca7e71dab078de22371b02f8881b81356b
Matt <scottm8@vcu.edu>
2020-03-26 20:17:43 -0400

    added more filter buttons and methods for each

--- DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/RecipientMain.java
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/RecipientMain.java
@@ -127,10 +127,135 @@ public class RecipientMain extends AppCompatActivity {
 			}
 		});
 
+		builder.show(); // methood to show the popup dialog
 
+	}
+
+
+
+	/*
+    Method for filter button and multiple choice popup dialog
+    consists of inner .setMultiChoiceItems() method for the checkbox list
+    .setPositivebutton() method for an 'ok' button
+    .setNeutralButton() method for a 'cancel' button
+     */
+	public void filterCondition(View view) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);    // the alert popup class
+		builder.setTitle("Category Filter"); // set title of dialog
+
+
+		final String[] options = { "Clothes", "Food", "Furniture", "Toiletries", "Games/Toys", "Books", "Electronics", "Other"};    // options for the popup
+		final boolean[] checked = new boolean[] { false, false, false, false, false, false, false, false };   // default state of checkboxes ( seems to be required )
+		final List<String> list = Arrays.asList(options);
+
+        /*
+        Method for the actual multiple choice checkbox popup, containing onlick listener
+        setMultiChoiceItems()
+         */
+		builder.setMultiChoiceItems(options, checked, new DialogInterface.OnMultiChoiceClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+
+				checked[which] = isChecked;
+				String currentItem = list.get(which);
+
+				Toast.makeText(RecipientMain.this,currentItem + " set to : " + isChecked, Toast.LENGTH_SHORT).show();
+
+
+			}
+		});
+
+
+        /*
+        Method for the ok button and onclick handler
+         */
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"ok button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
+        /*
+        Method for the cancel button and onclick handler (neutral for now)
+        there is also a setNegativeButton method
+         */
+
+		builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"cancel button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
 
 		builder.show(); // methood to show the popup dialog
 
+	}
+
+
+
+	/*
+    Method for filter button and multiple choice popup dialog
+    consists of inner .setMultiChoiceItems() method for the checkbox list
+    .setPositivebutton() method for an 'ok' button
+    .setNeutralButton() method for a 'cancel' button
+     */
+	public void filterDate(View view) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);    // the alert popup class
+		builder.setTitle("Category Filter"); // set title of dialog
+
+
+		final String[] options = { "Clothes", "Food", "Furniture", "Toiletries", "Games/Toys", "Books", "Electronics", "Other"};    // options for the popup
+		final boolean[] checked = new boolean[] { false, false, false, false, false, false, false, false };   // default state of checkboxes ( seems to be required )
+		final List<String> list = Arrays.asList(options);
+
+        /*
+        Method for the actual multiple choice checkbox popup, containing onlick listener
+        setMultiChoiceItems()
+         */
+		builder.setMultiChoiceItems(options, checked, new DialogInterface.OnMultiChoiceClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+
+				checked[which] = isChecked;
+				String currentItem = list.get(which);
+
+				Toast.makeText(RecipientMain.this,currentItem + " set to : " + isChecked, Toast.LENGTH_SHORT).show();
+
+
+			}
+		});
+
+
+        /*
+        Method for the ok button and onclick handler
+         */
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"ok button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
+        /*
+        Method for the cancel button and onclick handler (neutral for now)
+        there is also a setNegativeButton method
+         */
+
+		builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"cancel button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		builder.show(); // methood to show the popup dialog
 
 	}
 
@@ -139,15 +264,67 @@ public class RecipientMain extends AppCompatActivity {
 
 
 
+	/*
+    Method for filter button and multiple choice popup dialog
+    consists of inner .setMultiChoiceItems() method for the checkbox list
+    .setPositivebutton() method for an 'ok' button
+    .setNeutralButton() method for a 'cancel' button
+     */
+	public void filterDelivery(View view) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);    // the alert popup class
+		builder.setTitle("Category Filter"); // set title of dialog
 
 
+		final String[] options = { "Clothes", "Food", "Furniture", "Toiletries", "Games/Toys", "Books", "Electronics", "Other"};    // options for the popup
+		final boolean[] checked = new boolean[] { false, false, false, false, false, false, false, false };   // default state of checkboxes ( seems to be required )
+		final List<String> list = Arrays.asList(options);
+
+        /*
+        Method for the actual multiple choice checkbox popup, containing onlick listener
+        setMultiChoiceItems()
+         */
+		builder.setMultiChoiceItems(options, checked, new DialogInterface.OnMultiChoiceClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 
 
+				checked[which] = isChecked;
+				String currentItem = list.get(which);
+
+				Toast.makeText(RecipientMain.this,currentItem + " set to : " + isChecked, Toast.LENGTH_SHORT).show();
 
 
+			}
+		});
 
 
+        /*
+        Method for the ok button and onclick handler
+         */
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"ok button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
 
+
+        /*
+        Method for the cancel button and onclick handler (neutral for now)
+        there is also a setNegativeButton method
+         */
+
+		builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(RecipientMain.this,"cancel button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		builder.show(); // methood to show the popup dialog
+
+	}
 
 
 
