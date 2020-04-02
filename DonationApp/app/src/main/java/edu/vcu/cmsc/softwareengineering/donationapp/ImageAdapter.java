240083d4272f282a69716f8e9433a5e0e9268555
Megan Spiers <Spiers4.13@gmail.com>
2020-04-02 09:31:11 -0400

    Added ImageAdapter class

---
+++ DonationApp/app/src/main/java/edu/vcu/cmsc/softwareengineering/donationapp/ImageAdapter.java
@@ -0,0 +1,69 @@
+package edu.vcu.cmsc.softwareengineering.donationapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<newItemInfo> mUploads;
+
+    public ImageAdapter(Context context, List<newItemInfo> uploads){
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        newItemInfo itemCurrent = mUploads.get(position);
+        holder.textViewName.setText(itemCurrent.getItemDescription());
+        Picasso.with(mContext)
+                .load(itemCurrent.getItemImageUrl())
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            imageView = itemView.findViewById(R.id.image_view_upload);
+        }
+    }
+
+
+
+
+}
